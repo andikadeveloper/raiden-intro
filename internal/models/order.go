11c5ddc0428e1ad667577b16fad59f9903cac313
andikadeveloper <muhammadandika@refactory.id>
2024-06-03 18:29:12 +0700

    feat: add acl authenticated for order

--- internal/models/order.go
+++ internal/models/order.go
@@ -2,17 +2,18 @@ package models
 
 import (
 	"time"
+
 	"github.com/sev-2/raiden"
 )
 
 type Order struct {
 	raiden.ModelBase
-	Id int64 `json:"id,omitempty" column:"name:id;type:bigint;primaryKey;autoIncrement;nullable:false"`
+	Id        int64     `json:"id,omitempty" column:"name:id;type:bigint;primaryKey;autoIncrement;nullable:false"`
 	CreatedAt time.Time `json:"created_at,omitempty" column:"name:created_at;type:timestampz;nullable:false;default:now()"`
 
 	// Table information
 	Metadata string `json:"-" schema:"public" rlsEnable:"true" rlsForced:"false"`
 
 	// Access control
-	Acl string `json:"-" read:"" write:""`
+	Acl string `json:"-" read:"authenticated" write:"authenticated"`
 }
